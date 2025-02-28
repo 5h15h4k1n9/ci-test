2ab537baf357b8855b361548acfb433b3c08a1d9
Roman Shishkin <st077017@student.spbu.ru>
2025-02-28 06:32:13 +0300

    Testing release action 42

--- .github/scripts/delete-release.sh
+++ .github/scripts/delete-release.sh
@@ -1,4 +1,4 @@
 echo "Deleting release due to failure in previous jobs"
 curl -X DELETE \
   -H "Authorization: token $1" \
-  https://api.github.com/repos/"$2"/"$3"/releases/"$4"
+  https://api.github.com/repos/"$2"/releases/"$3"
