0bb5e90342678f18689d04b6d476f58b43cfdf71
Roman Shishkin <st077017@student.spbu.ru>
2025-02-28 09:39:39 +0300

    Testing release action 158

--- .github/scripts/extract-versions.sh
+++ .github/scripts/extract-versions.sh
@@ -8,9 +8,11 @@ fi
 
 MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2+1".0"}')
 PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2"."$3+1}')
+PATCH_BRANCH_NAME=$(echo "$PATCH_VERSION" | awk -F. '{print $1"."$2"}')
 
 {
   echo "VERSION=$VERSION";
   echo "MINOR_VERSION=$MINOR_VERSION";
   echo "PATCH_VERSION=$PATCH_VERSION";
+  echo "PATCH_BRANCH_NAME=$PATCH_BRANCH_NAME";
 } >> "$GITHUB_ENV"
