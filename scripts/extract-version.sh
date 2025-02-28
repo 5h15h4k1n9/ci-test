ee4211770edc9d5b556e813da0f8a5d151112a77
Roman Shishkin <st077017@student.spbu.ru>
2025-02-28 03:33:05 +0300

    Testing release action 11

--- scripts/extract-version.sh
+++ scripts/extract-version.sh
@@ -6,7 +6,7 @@ else
     VERSION=${TAG_NAME//-SNAPSHOT/}
 fi
 
-MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2".0"}')
+MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2+1".0"}')
 PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2"."$3+1}')
 
 echo "VERSION=$VERSION;MINOR_VERSION=$MINOR_VERSION;PATCH_VERSION=$PATCH_VERSION" >> "$GITHUB_ENV"
