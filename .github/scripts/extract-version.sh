340a41a4d9f8c94652a1021a26117d040db1b2b6
Roman Shishkin <st077017@student.spbu.ru>
2025-02-28 03:23:25 +0300

    Testing release action 9

---
+++ .github/scripts/extract-version.sh
@@ -0,0 +1,12 @@
+TAG_NAME=${{ github.event.release.tag_name }}
+
+if [[ $TAG_NAME =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    VERSION=${TAG_NAME:1}
+else
+    VERSION=${TAG_NAME//-SNAPSHOT/}
+fi
+
+MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2".0"}')
+PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2"."$3+1}')
+
+echo "VERSION=$VERSION;MINOR_VERSION=$MINOR_VERSION;PATCH_VERSION=$PATCH_VERSION" >> "$GITHUB_ENV"
