e42eb5a6df7cb186432e8ffdf42bf6af0cc9b3c6
Roman Shishkin <st077017@student.spbu.ru>
2025-03-03 00:57:14 +0300

    Testing release action 177

--- .github/scripts/extract-versions.sh
+++ .github/scripts/extract-versions.sh
@@ -6,13 +6,13 @@ else
     VERSION=${TAG_NAME//-SNAPSHOT/}
 fi
 
-MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2+1".0"}')
-PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2"."$3+1}')
-PATCH_BRANCH_NAME=$(echo "$VERSION" | awk -F. '{print $1"."$2}')
+NEXT_MINOR_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2+1".0"}')
+NEXT_PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2"."$3+1}')
+PATCH_VERSION=$(echo "$VERSION" | awk -F. '{print $1"."$2}')
 
 {
   echo "VERSION=$VERSION";
-  echo "MINOR_VERSION=$MINOR_VERSION";
+  echo "NEXT_MINOR_VERSION=$NEXT_MINOR_VERSION";
+  echo "NEXT_PATCH_VERSION=$NEXT_PATCH_VERSION";
   echo "PATCH_VERSION=$PATCH_VERSION";
-  echo "PATCH_BRANCH_NAME=$PATCH_BRANCH_NAME";
 } >> "$GITHUB_ENV"
