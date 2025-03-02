b67d80ec9d8e3493b3418c6107861c9284b38695
Roman Shishkin <st077017@student.spbu.ru>
2025-03-02 23:46:18 +0000

    release: Update version to 31.0.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ package com.trikset
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "%CURRENT_VERSION%"
+    val name = "31.0.0"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
