41f50f877d4244c1b9a27da47811e47f1256e46f
Roman Shishkin <st077017@student.spbu.ru>
2025-03-03 02:31:39 +0300

    Testing release action 190

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,6 @@ package com.trikset
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val a = "20.0.0"
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
