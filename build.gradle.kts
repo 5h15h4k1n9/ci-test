301e6bfd0bcedf5518a926e8a69e8f3f909277c5
Roman Shishkin <st077017@student.spbu.ru>
2025-03-03 03:55:47 +0300

    Testing release action 200

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
+    jacoco
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.google.protobuf") version "0.9.4"
     kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
-
 group = "com.trikset"
 version = "35.2.0-snapshot"
 
