c21875d684985ce59c84c3e461d5b4abe11cfed1
Roman Shishkin <st077017@student.spbu.ru>
2025-03-03 03:00:41 +0300

    Testing release action 196

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,7 @@ package com.trikset
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "31.0.0"
+    val currentVersion = "%CURRENT_VERSION%"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
