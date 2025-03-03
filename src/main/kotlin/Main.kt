6b9a75b46be4263c5d053aa8556734e38b7a67f0
Roman Shishkin <st077017@student.spbu.ru>
2025-03-03 00:23:26 +0000

    release: Update version to 35.0.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package com.trikset
 
 /**
- * @since %CURRENT_VERSION%
+ * @since 35.0.2
  */
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
